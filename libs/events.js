85a7243520dbcd29ea3cfd260022c7ae55486569
simonpartridge86 <simonpartridge86@gmail.com>
2022-06-24 11:06:53 +0100

    final testing

--- libs/events.js
+++ libs/events.js
@@ -1,50 +1,54 @@
 const events = [
     {
-        
         type: "Social",
         author: "Simon",
-        description: "Just a meet up and chat after class",
-        date: "2022-07-01",
-        start_time: "17:30",
-        end_time: "18:30",
+        description: "Just want to meet up and chat after class on Monday",
+        date: "2022-06-27",
+        start_time: "17:15",
+        end_time: "18:15",
         social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
         location: "",
         attendance: 3,
         status: true
-
-
     },
 
     {
-        
         type: "Games Session",
         author:"Matt",
-        description: "Poker game, anyone?",
-        date: "2022-06-29",
+        description: "Poker game, anyone? Just for fun, not for money.",
+        date: "2022-06-28",
         start_time: "18:30",
         end_time: "20:30",
         social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
         location: "",
         attendance: 6,
         status: true
-
-
     },
 
     {
-        
-        type: "Coding Help",
+
+        type: "Group Coding",
         author: "Kunal",
-        description: "Need help passing props to components in React",
-        date: "2022-06-25",
+        description: "I'll be practicing more with React this weekend, feel free to join me",
+        date: "2022-06-26",
         start_time: "18:00",
         end_time: "19:30",
         social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
         location: "",
         attendance: 4,
         status: true
-
-
+    },
+    {
+        type: "Social",
+        author: "Wanteng",
+        description: "I'd just like to chat about how the project is going for everyone",
+        date: "2022-06-23",
+        start_time: "18:00",
+        end_time: "19:30",
+        social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
+        location: "",
+        attendance: 8,
+        status: true
     }
 ]
 
