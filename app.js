c3ee6bbc1dd84b40c85654925e4298fcdd9c40f3
simonpartridge86 <simonpartridge86@gmail.com>
2022-10-24 19:24:53 +0100

    port updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 export const app = express();
 
-const PORT = process.env.PGPORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
