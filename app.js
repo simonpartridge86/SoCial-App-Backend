9b2794a94c48d1b40de761b822d46213eec57efe
simonpartridge86 <simonpartridge86@gmail.com>
2022-10-24 16:32:54 +0100

    mock data updated

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ export const app = express();
 
 const PORT = 3001;
 
-
 app.use(cors());
 app.use(express.json());
-app.use("/events",router);
-
+app.use("/events", router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-
-
--- libs/events.js
+++ libs/events.js
@@ -1,47 +1,52 @@
 const events = [
-    {
-        type: "Social",
-        author: "Wanteng",
-        description: "Just want to meet up and chat after class on Monday",
-        date: "2022-07-15",
-        start_time: "17:15",
-        end_time: "18:15",
-        social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
-        attendance: 3,
-    },
+  {
+    type: "Social",
+    author: "Wanteng",
+    description: "Just want to meet up and chat after class on Monday",
+    date: "2023-07-15",
+    start_time: "17:15",
+    end_time: "18:15",
+    social_link:
+      "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
+    attendance: 3,
+  },
 
-    {
-        type: "Games Session",
-        author:"Matt",
-        description: "Poker game, anyone? Just for fun, not for money.",
-        date: "2022-07-16",
-        start_time: "18:30",
-        end_time: "20:30",
-        social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
-        attendance: 6,
-    },
+  {
+    type: "Games Session",
+    author: "Matt",
+    description: "Poker game, anyone? Just for fun, not for money.",
+    date: "2023-07-16",
+    start_time: "18:30",
+    end_time: "20:30",
+    social_link:
+      "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
+    attendance: 6,
+  },
 
-    {
+  {
+    type: "Group Coding",
+    author: "Kunal",
+    description:
+      "I'll be practicing more with React this weekend, feel free to join me",
+    date: "2023-07-17",
+    start_time: "18:00",
+    end_time: "19:30",
+    social_link:
+      "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
+    attendance: 4,
+  },
+  {
+    type: "Social",
+    author: "Wanteng",
+    description:
+      "I'd just like to chat about how the project is going for everyone",
+    date: "2023-07-18",
+    start_time: "18:00",
+    end_time: "19:30",
+    social_link:
+      "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
+    attendance: 8,
+  },
+];
 
-        type: "Group Coding",
-        author: "Kunal",
-        description: "I'll be practicing more with React this weekend, feel free to join me",
-        date: "2022-07-17",
-        start_time: "18:00",
-        end_time: "19:30",
-        social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
-        attendance: 4,
-    },
-    {
-        type: "Social",
-        author: "Wanteng",
-        description: "I'd just like to chat about how the project is going for everyone",
-        date: "2022-07-18",
-        start_time: "18:00",
-        end_time: "19:30",
-        social_link: "https://us04web.zoom.us/j/8961811674?pwd=OWtYSU5kRDIvS2RHZ0RWanFheFNmUT09",
-        attendance: 8,
-    }
-]
-
-export default events
+export default events;
